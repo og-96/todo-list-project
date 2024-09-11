13d7e9972dcfaf812b39159cab4913e979ebac13
Jery <jerome.elsasser@bluewin.ch>
2024-09-11 17:22:14 +0200

    init todo

--- src/App.svelte
+++ src/App.svelte
@@ -2,48 +2,53 @@
   import svelteLogo from "./assets/svelte.svg";
   import viteLogo from "/vite.svg";
   import Counter from "./lib/Counter.svelte";
+
+  let todos = [];
+
+  let newTodo = "";
+
+  function addTodo() {
+    if (newTodo.trim() === "") return;
+    todos = [...todos, { text: newTodo, done: false }];
+    newTodo = "";
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
 </script>
 
 <main>
+  <header>
+    <h1>To Do List</h1>
+  </header>
+
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>To Do List</h1>
-
-  <div class="card">
-    <Counter />
+    <p></p>
   </div>
 
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
+  <section>
+    <input type="text" name="todo" id="todoInput" bind:value={newTodo} />
+    <button on:click={addTodo}>Add</button>
+  </section>
 
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+  <section>
+    <ul>
+      {#each todos as todo, index}
+        <li>
+          <input type="text" bind:value={todo.text} />
+          <button>X</button>
+        </li>
+      {/each}
+    </ul>
+  </section>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  input {
+    width: 400px;
+    height: 32px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  ul {
+    list-style-type: none;
   }
 </style>
